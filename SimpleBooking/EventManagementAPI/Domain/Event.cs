9efc30d3d4d70ef25aaaf28be0cd7e6262d9b4cc
tigeringarden <steventranucsd@gmail.com>
2018-08-05 16:55:11 -0700

    add Domain objects

---
+++ SimpleBooking/EventManagementAPI/Domain/Event.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventManagementAPI.Domain
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public EventPriceType PriceType { get; set; }
+        public EventCategory Category { get; set; }
+        public DateTime Date { get; set; }
+        public Decimal Price { get; set; }
+        public string ImageURL { get; set; }
+
+        public int PlaceId { get; set; }
+        public virtual Place Place { get; set; }
+
+        public int TicketId { get; set; }
+        public virtual IEnumerable<Ticket> Ticket { get; set; }
+    }
+}
