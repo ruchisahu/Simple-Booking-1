8104b223f86ff403630c086ffae4ae17a8eb4204
tigeringarden <steventranucsd@gmail.com>
2018-08-06 04:05:14 -0700

    add public endpoints to EventsController.cs in EventManagementAPI

---
+++ SimpleBooking/EventManagementAPI/Controllers/EventsController.cs
@@ -0,0 +1,116 @@
+﻿using EventManagementAPI.Data;
+using EventManagementAPI.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EventManagementAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Events")]
+    public class EventsController : Controller
+    {
+        private readonly ManagementContext managementContext;
+
+        public EventsController(ManagementContext managementContext)
+        {
+            this.managementContext = managementContext;
+        }
+
+        [HttpGet]
+        [Route("Launch")]
+        public IActionResult Launch()
+        {
+            return Ok("You have successfully reached the Event Management API!");
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetEvent(int id)
+        {
+            Event _event = managementContext.Events.Find(id);
+
+            if(_event == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_event);
+        }
+
+        [HttpGet]
+        [Route("{id}/Stats/SalesTotal")]
+        public IActionResult GetEventSalesTotal(int id)
+        {
+            int count = 0;
+            decimal totalAmount = 0;
+            Event _event = managementContext.Events.SingleOrDefault(x => x.Id == id); //event is a keyword hence the underscore
+
+            if(_event == null)
+            {
+                return NotFound();
+            }
+
+            foreach(Ticket ticket in _event.Tickets)
+            {
+                foreach(Transaction transaction in ticket.Transactions)
+                {
+                    count++;
+                    totalAmount += transaction.TotalAmount;
+                }
+            }
+
+            return Ok(Tuple.Create<int, decimal>(count, totalAmount));
+        }
+
+        [HttpGet]
+        [Route("{id}/Stats/SalesHistory")]
+        public IActionResult GetEventSalesHistory(int id)
+        {
+            Dictionary<DateTime, int> salesHistory = new Dictionary<DateTime, int>();
+            Event _event = managementContext.Events.SingleOrDefault(x => x.Id == id);
+
+            if (_event == null)
+            {
+                return NotFound();
+            }
+
+            foreach (Ticket ticket in _event.Tickets)
+            {
+                foreach (Transaction transaction in ticket.Transactions)
+                {
+                    DateTime date = transaction.ProcessingTime.Date;
+
+                    if (!salesHistory.ContainsKey(date))
+                    {
+                        salesHistory[date]++;
+                    }
+                    else
+                    {
+                        salesHistory.Add(date, 1);
+                    }
+                }
+            }
+
+            return Ok(salesHistory);
+        }
+
+        [HttpPut]
+        [Route("{id}/Edit/")]
+        public IActionResult EditEvent(Event _event)
+        {
+            Event match = managementContext.Events.SingleOrDefault(x => x.Id == _event.Id);
+
+            if (match == null)
+            {
+                return NotFound();
+            }
+
+            match = _event;
+            managementContext.SaveChanges();
+
+            return Ok(match);
+        }
+    }
+}
