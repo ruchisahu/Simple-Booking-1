7666ac311b4202b4640481ba05821ae9c32199d2
Madhura Pattekar <31863783+wpmapat@users.noreply.github.com>
2018-08-07 08:15:56 -0700

    Unsuccessful attempt at DockerCompose

--- SimpleBooking/OrderProcessorAPI/Startup.cs
+++ SimpleBooking/OrderProcessorAPI/Startup.cs
@@ -26,8 +26,16 @@ namespace OrderProcessorAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            //string connectionString=
+            var server = Configuration["DatabaseServer"];
+            var database = Configuration["DatabaseName"];
+            var user = Configuration["DatabaseUser"];
+            var password = Configuration["DatabaseUserPassword"];
+            var connectionString = String.Format("Server={0};Database={1};User={2};Password={3};", server, database, user, password);
+
             services.AddDbContext<OrderContext>
-                (options => options.UseSqlServer(Configuration["ConnectionString"]));
+                (options => options.UseSqlServer(connectionString));
 
             //Add Framework services
             services.AddSwaggerGen(options =>
