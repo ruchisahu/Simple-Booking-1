608ce10f053227efac361772ac837ae6bad4470a
Madhura Pattekar <31863783+wpmapat@users.noreply.github.com>
2018-08-12 20:58:40 -0700

    8/12

---
+++ WebMvcClient/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvcClient
+{
+    public class AppSettings
+    {
+        public string OrderUrl { get; set; }
+    }
+}
---
+++ WebMvcClient/ViewModels/OrderViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvcClient.Models
+{
+    public class OrderViewModel
+    {
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+        public DateTime EventDate { get; set; }
+        public string EventLocation { get; set; }
+        public decimal EventPrice { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public string UserFirstname { get; set; }
+        public string UserLastName { get; set; }
+        public string TelephoneNo { get; set; }
+        public string CountryName { get; set; }
+        public string Address { get; set; }
+        public string Address1 { get; set; }
+        public string CityName { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+
+    }
+}
