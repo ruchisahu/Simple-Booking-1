14b8c99209a9ee83896d6950f53a32b041e5dd18
Madhura Pattekar <31863783+wpmapat@users.noreply.github.com>
2018-08-29 23:50:33 -0700

    IntroducedMessagingCodeInOrderAPI

--- SimpleBooking/OrderProcessorAPI/Startup.cs
+++ SimpleBooking/OrderProcessorAPI/Startup.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -12,6 +15,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using OrderProcessorAPI.Data;
+using RabbitMQ.Client;
 
 namespace OrderProcessorAPI
 {
@@ -23,9 +27,11 @@ namespace OrderProcessorAPI
         }
 
         public IConfiguration Configuration { get; }
+        public IContainer ApplicationContainer { get; private set; }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
 
@@ -60,13 +66,35 @@ namespace OrderProcessorAPI
             services.AddCors(options =>
             {
                 options.AddPolicy("CorsPolicy",
-                    builder => builder.AllowAnyOrigin()
+                    poly=> poly.AllowAnyOrigin()
                     .AllowAnyMethod()
                     .AllowAnyHeader()
                     .AllowCredentials());
             });
+            var builder = new ContainerBuilder();
+            builder.Register(c =>
+            {
+                return Bus.Factory.CreateUsingRabbitMq(rmq =>
+                {
+                    rmq.Host(new Uri("rabbitmq://rabbitmq"), "/", h =>
+                    {
+                        h.Username("guest");
+                        h.Password("guest");
+                    });
+                    rmq.ExchangeType = ExchangeType.Fanout;
+                });
 
+            }).
+             As<IBusControl>()
+            .As<IBus>()
+            .As<IPublishEndpoint>()
+            .SingleInstance();
+
+            builder.Populate(services);
+            ApplicationContainer = builder.Build();
+            return new AutofacServiceProvider(ApplicationContainer);
         }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
